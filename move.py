6008a658cb3b2ca1542fc578cad17f1d32d77b54
Joe Tay <tay.joe@gmail.com>
2023-08-21 22:08:26 +0800

    Update move.py

--- move.py
+++ move.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle 
 import mediapipe as mp
 from landmarks import landmarks
-import time
+import cv2
 from PIL import Image, ImageDraw, ImageFont
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline 
@@ -27,144 +27,93 @@ coords = './data/coords.csv'
 num_coords = 33
 
 
-if "pose" not in st.session_state:
-	st.session_state.pose = ""
-else:
-	pose_name = st.session_state.pose
-
-def calculate_angle(a,b,c): #calculation of the angles of the joints
-	a = np.array(a) # First
-	b = np.array(b) # Mid
-	c = np.array(c) # End
-	
-	radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-	angle = np.abs(radians*180.0/np.pi)
-	
-	if angle >180.0:
-		angle = 360-angle
-		
-	return angle
-
-
-# countdown timer 
-def countdown(t):
-	while t: # while t > 0 for clarity 
-		mins = t // 60
-		secs = t % 60
-		timer = '{:02d}:{:02d}'.format(mins, secs)
-		print(timer, end="\r") # overwrite previous line 
-		time.sleep(1)
-		t -= 1
-
-def reset_counter(): 
-	global counter
-	counter = 0 
-
 with open('deadlift.pkl', 'rb') as f: 
 	model = pickle.load(f)
 
 def callback(frame):
-    global current_stage
-    global counter
-    global bodylang_class
-    global bodylang_prob
+	global current_stage
+	global counter
+	global bodylang_class
+	global bodylang_prob
 
-    image = frame.to_ndarray(format="bgr24")
+	image = frame.to_ndarray(format="bgr24")
+	results = pose.process(image)
+	if results.pose_landmarks:
+		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
+			mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius = 2), 
+			mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius = 2)) 
 
-    results = pose.process(image)
-    if results.pose_landmarks:
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius = 2),
-                                  mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius = 2))
+		try: 
+			row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
+			# pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+			X = pd.DataFrame([row], columns = landmarks) 
+			bodylang_prob = model.predict_proba(X)[0]
+			bodylang_class = model.predict(X)[0]
+			if bodylang_class =="down" and bodylang_prob[bodylang_prob.argmax()] > 0.7: 
+				current_stage = "down" 
+			elif current_stage == "down" and bodylang_class == "up" and bodylang_prob[bodylang_prob.argmax()] > 0.7:
+				current_stage = "up" 
+				counter += 1 
+			image = cv2.putText(image,bodylang_class + ": " + str(bodylang_prob[bodylang_prob.argmax()]), (00,20), #the webcam resolution
+					   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        try:
-            row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
-            X = pd.DataFrame([row], columns = landmarks)
-            bodylang_prob = model.predict_proba(X)[0]
-            bodylang_class = model.predict(X)[0]
-            if bodylang_class == "down" and bodylang_prob[bodylang_prob.argmax()] > 0.7:
-                current_stage = "down"
-            elif current_stage == "down" and bodylang_class == "up" and bodylang_prob[bodylang_prob.argmax()] > 0.7:
-                current_stage = "up"
-                counter += 1
-            text = bodylang_class + ": " + str(bodylang_prob[bodylang_prob.argmax()])
-            # Convert the image to RGB for PIL
-            image_pil = Image.fromarray(image[..., ::-1])
-            draw = ImageDraw.Draw(image_pil)
-            font = ImageFont.load_default()  # Provide the path to your font file if not in default location
-            draw.text((0, 0), text, font=font, fill=(255, 255, 255))
-            # Convert the image back to BGR
-            image = np.array(image_pil)[:, :, ::-1]
+		except Exception as e: 
+			print(e)
+	else:
+		print("No landmarks found") 
 
-        except Exception as e:
-            print(e)
-    else:
-        print("No landmarks found")
-
-    img = image[:, :500, :]
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+	img = image[:, :500, :] 
+	
+	return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
 def analysis_callback(frame, model):
-    image = frame.to_ndarray(format="bgr24")
-    results = pose.process(image)
-    if results.pose_landmarks:
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius = 2),
-                                  mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius = 2))
+	image = frame.to_ndarray(format="bgr24")
+	results = pose.process(image)
+	if results.pose_landmarks:
+		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
+			mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius = 2), 
+			mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius = 2)) 
 
-        try:
-            row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
-            X = pd.DataFrame([row], columns=landmarks)
-            bodylang_prob = model.predict_proba(X)[0]
-            bodylang_class = model.predict(X)[0]
-            text = bodylang_class + ": " + str(bodylang_prob[bodylang_prob.argmax()])
-            # Convert the image to RGB for PIL
-            image_pil = Image.fromarray(image[..., ::-1])
-            draw = ImageDraw.Draw(image_pil)
-            font = ImageFont.load_default()
-            draw.text((0, 0), text, font=font, fill=(255, 255, 255))
-            # Convert the image back to BGR
-            image = np.array(image_pil)[:, :, ::-1]
+		try: 
+			row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
+			# pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+			X = pd.DataFrame([row], columns = landmarks) 
+			bodylang_prob = model.predict_proba(X)[0]
+			bodylang_class = model.predict(X)[0]
+			image = cv2.putText(image,bodylang_class + ": " + str(bodylang_prob[bodylang_prob.argmax()]), (00,20), #the webcam resolution
+					   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        except Exception as e:
-            print(e)
-    else:
-        print("No landmarks found")
+		except Exception as e: 
+			print(e)
+	else:
+		print("No landmarks found") 
 
-    img = image[:, :500, :]
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+	img = image[:, :500, :] 
+	
+	return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 def photo_callback(image, model):
-    results = pose.process(image)
-    if results.pose_landmarks:
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius = 2),
-                                  mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius = 2))
+	results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+	if results.pose_landmarks:
+		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
+			mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius = 2), 
+			mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius = 2)) 
 
-        try:
-            row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
-            X = pd.DataFrame([row], columns=landmarks)
-            bodylang_prob = model.predict_proba(X)[0]
-            bodylang_class = model.predict(X)[0]
-            text = bodylang_class + ": " + str(bodylang_prob[bodylang_prob.argmax()])
-            # Convert the image to RGB for PIL
-            image_pil = Image.fromarray(image[..., ::-1])
-            draw = ImageDraw.Draw(image_pil)
-            font = ImageFont.load_default()
-            draw.text((0, 0), text, font=font, fill=(255, 255, 255))
-            # Convert the image back to BGR
-            image = np.array(image_pil)[:, :, ::-1]
+		try: 
+			row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
+			X = pd.DataFrame([row], columns=landmarks) 
+			bodylang_prob = model.predict_proba(X)[0]
+			bodylang_class = model.predict(X)[0]
+			image = cv2.putText(image, bodylang_class + ": " + str(bodylang_prob[bodylang_prob.argmax()]), (0,20),
+					   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+		except Exception as e: 
+			print(e)
+	else:
+		print("No landmarks found")
 
-        except Exception as e:
-            print(e)
-    else:
-        print("No landmarks found")
-
-    img = image[:, :500, :]
-    return img  # , bodylang_class, bodylang_prob
+	img = image[:, :500, :]
+	return img#, bodylang_class, bodylang_prob
 
 
 def train_callback(frame, pose_name):
